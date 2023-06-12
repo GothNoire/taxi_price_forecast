b156727d4e92261c6f7e4efeab935e0be8ade59a
Danila Rostovtsev <rostovtsev@hypeauditor.com>
2023-06-12 18:52:07 +0400

    fix: codestyle

--- openweather.py
+++ openweather.py
@@ -1,6 +1,7 @@
-import requests
-from dotenv import load_dotenv
 import os
+import requests
+
+from dotenv import load_dotenv
 
 path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(path):
@@ -8,7 +9,7 @@ if os.path.exists(path):
 appid = os.getenv("WEATHER_KEY_API")
 
 
-def get_weather_data (latitude: float, longitude: float) -> dict:
+def get_weather_data(latitude: float, longitude: float) -> dict:
     url = 'https://api.openweathermap.org/data/2.5/weather'
     params = {
         'lat': latitude,
@@ -21,22 +22,22 @@ def get_weather_data (latitude: float, longitude: float) -> dict:
     return response.json()
 
 
-def is_rainy (data: dict) -> int:
+def is_rainy(data: dict) -> int:
     return int('rain' in data)
 
 
-def is_snowy (data: dict) -> int:
+def is_snowy(data: dict) -> int:
     return int('snow' in data)
 
 
-def get_current_temp (data: dict) -> float:
+def get_current_temp(data: dict) -> float:
     return data['main']['temp']
 
 
-def get_cloud_percent (data: dict) -> int:
+def get_cloud_percent(data: dict) -> int:
     return data['clouds']['all']
 
 
-def get_wind_speed (data: dict) -> float:
+def get_wind_speed(data: dict) -> float:
     return data['wind']['speed']
 

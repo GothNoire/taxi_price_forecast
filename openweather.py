e3a4d6bc09989ff30bb4b26726be6ebd16e6e4b2
GothNoire <ruzanow.n@yandex.ru>
2023-08-06 22:15:10 +0400

    add forecast weather

--- openweather.py
+++ openweather.py
@@ -1,12 +1,30 @@
-import os
 import requests
-
 from dotenv import load_dotenv
+import os
+from datetime import timedelta, datetime
 
 path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(path):
     load_dotenv(path)
 appid = os.getenv("WEATHER_KEY_API")
+rapid_id = os.getenv("WEATHER_KEY_RAPID")
+
+
+def get_forecast_weather_data(lat, lon, days):
+    url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+    params = {"q": f"{lat},{lon}", "days": days}
+
+    headers = {
+        "X-RapidAPI-Key": rapid_id,
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
+
+
+def get_forecast_weather_by_date(forecast_weather_data, weather_param, day):
+    hour = datetime.strftime(forecast_weather_data, '%H')
+    return forecast_weather_data['forecast']['forecastday'][day]['hour'][hour][weather_param] if weather_param in forecast_weather_data['forecast']['forecastday'][day]['hour'][hour] else None
 
 
 def get_weather_data(latitude: float, longitude: float) -> dict:
@@ -40,4 +58,3 @@ def get_cloud_percent(data: dict) -> int:
 
 def get_wind_speed(data: dict) -> float:
     return data['wind']['speed']
-
